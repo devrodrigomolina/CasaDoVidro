7c8d8c24dbd06782f71ba466f4cb66f67ef5e4f1
FOXXX <75998678+FOXXX2K@users.noreply.github.com>
2022-08-03 00:28:23 -0300

    Update: Ajustes nos tamanhos do header

--- components/HeaderPrincipal.vue
+++ components/HeaderPrincipal.vue
@@ -9,10 +9,11 @@
     </div>
     <nav>
       <ul>
-        <li>Inicio</li>
-        <li>Sobre</li>
-        <li>Serviços</li>
-        <li>Contato</li>
+        <li><a href="">Inicio</a></li>
+        <li><a href="">Sobre</a></li>
+        <li><a href="">Serviços</a></li>
+        <li><a href="">Contato</a></li>
+        <li><a href="">Contato</a></li>
       </ul>
     </nav>
   </div>
@@ -27,13 +28,12 @@ export default {
 <style scoped>
 .container-header-principal {
   width: 100vw;
-  max-width: 1400px;
+  max-width: 1200px;
   display: flex;
-  justify-content: space-around;
+  justify-content: space-evenly;
   align-items: center;
   height: 80px;
   margin: 0 auto;
-  border: 1px solid red;
 }
 .logo {
   width: 80px;
@@ -43,14 +43,41 @@ export default {
   height: 100%;
 }
 .redes-sociais {
+  width: 140px;
+  height: 100%;
   display: flex;
+  align-items: center;
+}
+.redes-sociais i {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 80px;
+  height: 60px;
+  border-radius: 8px;
+  font-size: 22px;
+  font-weight: 500;
+  transition: all .5s;
+}
+.redes-sociais i:hover {
+  background: #309ac7;
+  color: white;
 }
 nav {
   width: 500px;
+  font-weight: 500;
 }
 ul {
   display: flex;
   justify-content: space-between;
   align-items: center;
+}
+a {
+  color: #0b0e11;
+  font-family: 'Montserrat';
+  font-size: 1.1rem;
+}
+a:hover {
+  color: #309ac7;
 }
 </style>
