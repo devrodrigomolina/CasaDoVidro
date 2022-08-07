bd9ccf984b8871ff3e178ae7fdf4b8d792b25165
FOXXX <75998678+FOXXX2K@users.noreply.github.com>
2022-08-07 03:39:40 -0300

    Corrigido bug no carrossel

--- components/Carrossel.vue
+++ components/Carrossel.vue
@@ -5,11 +5,17 @@
       <span class="arrow-rigth">&#8680;</span>
     </div>
     <div class="overlay"></div>
-    <div class="carrossel">
-      <transition :appear="true">
-        <img :src="images[settings.imageIndex]" alt="">
-      </transition>
+
+    <div class="a">
+      <CarrosselItens
+        v-for="(img, index) in images"
+        :key="img"
+        :slide="img"
+        :imageIndex="settings.imageIndex"
+        :index="index"
+      />
     </div>
+
   </div>
 </template>
 
@@ -20,50 +26,55 @@ export default {
       settings: {
         imageIndex: 0,
         autoplay: true,
-        autoplaySpeed: 2000,
+        autoplaySpeed: null,
       },
       images: [
-        require('@/assets/images/carrossel-1.jpg'),
-        require('@/assets/images/carrossel-2.jpg'),
-        require('@/assets/images/carrossel-3.jpg'),
-        require('@/assets/images/carrossel-4.jpg'),
-        require('@/assets/images/carrossel-5.jpg'),
-        require('@/assets/images/carrossel-6.jpg')
-      ]
-    }
+        require("@/assets/images/carrossel-1.jpg"),
+        require("@/assets/images/carrossel-2.jpg"),
+        require("@/assets/images/carrossel-3.jpg"),
+        require("@/assets/images/carrossel-4.jpg"),
+        require("@/assets/images/carrossel-5.jpg"),
+        require("@/assets/images/carrossel-6.jpg"),
+      ],
+    };
   },
   methods: {
     backCarrossel() {
-      if(this.settings.imageIndex >= this.images.length -1) {
-        this.settings.imageIndex = 0
-      }else {
-        this.settings.imageIndex++
-      } 
-    }
-  }
-}
+      if (this.settings.imageIndex >= this.images.length - 1) {
+        this.settings.imageIndex = 0;
+      } else {
+        this.settings.imageIndex++;
+      }
+    },
+    setImageIndex(index) {
+      this.settings.imageIndex = index;
+    },
+  },
+
+  mounted() {
+    this.settings.autoplaySpeed = setInterval(() => {
+      const index = this.settings.imageIndex < this.images.length - 1 ? this.settings.imageIndex + 1 : 0;
+      this.setImageIndex(index);
+    }, 5000);
+  },
+  beforeUnmount() {
+    clearInterval(this.settings.autoplaySpeed);
+  },
+};
 </script>
 
 <style>
-.v-enter, .v-leave-to {
-
-}
-.v-enter-to, .v-leave{
-  transform: translateX(-100%);
-}
-.v-enter-active, .v-leave-active {
-  transition: all 3s;
-}
-
 .container-carrossel {
-  display: flex;
-  overflow: hidden ;
-}
-.carrossel {
   width: 100vw;
   height: 500px;
-  transition: all 3s;
+  position: absolute;
+  overflow: hidden;
 }
+.a { 
+  display: flex;
+  overflow: hidden;
+}
+
 .arrows {
   width: 100vw;
   height: 500px;
@@ -74,24 +85,17 @@ export default {
   align-items: center;
   position: absolute;
 }
-.arrow-left, .arrow-rigth {
+.arrow-left,
+.arrow-rigth {
   font-size: 3rem;
   color: white;
-  z-index: 2;
+  z-index: 12;
 }
 .overlay {
   width: 100vw;
   height: 500px;
-  z-index: 1;
+  z-index: 4;
   position: absolute;
-  background-color: rgba(0,0,0,.5)
+  background-color: rgba(0, 0, 0, 0.5);
 }
-.carrossel img {
-  width: 100vw;
-  height: 100%;
-  background-position: center;
-  object-fit: cover;
-  transition: all 3s;
-}
-
 </style>
---
+++ components/CarrosselItens.vue
@@ -0,0 +1,42 @@
+<template>
+  <transition name="carrossel" >
+    <div class="carrossel" v-show="index == imageIndex  ">
+      <img :src="slide" alt="" />
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  props: {
+    slide: Array, String,
+    index: Number,
+    imageIndex: Number,
+  },
+}
+</script>
+
+<style scoped>
+.carrossel-enter-active,
+.carrossel-leave-active {
+  transition: all 3s ease-in-out;
+}
+.carrossel-enter-from {
+  transform: translateX(-100%);
+}
+.carrossel-leave-to {
+  transform: translateX(100%);
+  z-index: 2;
+}
+
+.carrossel {
+  width: 100vw;
+  height: 500px;
+  position: absolute;
+}
+.carrossel img {
+  width: 100vw;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
