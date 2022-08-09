84e03b0bad24fd7bfe9619d0511ab712111c1b9a
FOXXX <75998678+FOXXX2K@users.noreply.github.com>
2022-08-08 22:28:50 -0300

    Create: Componente footer

---
+++ components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container-footer">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.container-footer {
+  width: 100vw;
+  height: 400px;
+  background: #0b232e;
+}
+</style>
