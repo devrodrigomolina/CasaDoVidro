071a133588171820ae676c5cb2af2081421c1d08
FOXXX <75998678+FOXXX2K@users.noreply.github.com>
2022-08-08 22:29:03 -0300

    Refatoração de codigo

--- components/NossosServicos.vue
+++ components/NossosServicos.vue
@@ -5,22 +5,50 @@
       <h2>SERVIÇOS</h2>
     </div>
     <div class="servicos-itens">
-
       <div class="item">
         <div class="infos-img">
           <div class="info">
             <h2>Box de vidro</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat omnis explicabo et dolor itaque hic placeat quis quo in esse provident pariatur, totam, nulla ratione consectetur eos reprehenderit. Cum, in! Lorem ipsum dolor sit amet consectetur adipisicing elit. Et laudantium ratione odit ad laboriosam error quidem, animi, illo quia adipisci, reprehenderit quibusdam. Impedit voluptatibus repellendus tempore beatae rerum quod consequatur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, animi, dignissimos numquam voluptatibus dolorem et velit temporibus harum esse blanditiis magni neque assumenda omnis ex placeat. Ab temporibus ducimus necessitatibus!</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat
+              omnis explicabo et dolor itaque hic placeat quis quo in esse
+              provident pariatur, totam, nulla ratione consectetur eos
+              reprehenderit. Cum, in! Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Et laudantium ratione odit ad laboriosam error
+              quidem, animi, illo quia adipisci, reprehenderit quibusdam.
+              Impedit voluptatibus repellendus tempore beatae rerum quod
+              consequatur! Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Vel, animi, dignissimos numquam voluptatibus dolorem et
+              velit temporibus harum esse blanditiis magni neque assumenda omnis
+              ex placeat. Ab temporibus ducimus necessitatibus!
+            </p>
+            <div class="btn-container">
+              <button class="btn">Saiba Mais</button>
+            </div>
           </div>
-          <img src="@/assets/images/box.jpg" alt="">
+          <img src="@/assets/images/box.jpg" alt="" />
         </div>
       </div>
       <div class="item">
         <div class="infos-img">
-          <img src="@/assets/images/corrimao.jpg" alt="">
+          <img src="@/assets/images/corrimao.jpg" alt="" />
           <div class="info">
             <h2>Corrimão de vidro</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat omnis explicabo et dolor itaque hic placeat quis quo in esse provident pariatur, totam, nulla ratione consectetur eos reprehenderit. Cum, in! Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quia accusantium, pariatur sunt, voluptas odit, dolorem dolor iusto a inventore tempora illo facilis quis iste! Quae deserunt id minima beatae! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas neque veritatis, at nemo odio recusandae quidem illum voluptate officiis pariatur, omnis minima praesentium vitae iure. Eaque voluptates labore sint amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat
+              omnis explicabo et dolor itaque hic placeat quis quo in esse
+              provident pariatur, totam, nulla ratione consectetur eos
+              reprehenderit. Cum, in! Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Non quia accusantium, pariatur sunt, voluptas
+              odit, dolorem dolor iusto a inventore tempora illo facilis quis
+              iste! Quae deserunt id minima beatae! Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Quas neque veritatis, at nemo odio
+              recusandae quidem illum voluptate officiis pariatur, omnis minima
+              praesentium vitae iure. Eaque voluptates labore sint amet.
+            </p>
+            <div class="btn-container">
+              <button class="btn">Saiba Mais</button>
+            </div>
           </div>
         </div>
       </div>
@@ -28,26 +56,34 @@
         <div class="infos-img">
           <div class="info">
             <h2>Espelho</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat omnis explicabo et dolor itaque hic placeat quis quo in esse provident pariatur, totam, nulla ratione consectetur eos reprehenderit. Cum, in! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam numquam pariatur itaque, vel quis sequi magni harum minus nulla molestias quibusdam veritatis corporis excepturi architecto non dolore nisi ratione expedita.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat
+              omnis explicabo et dolor itaque hic placeat quis quo in esse
+              provident pariatur, totam, nulla ratione consectetur eos
+              reprehenderit. Cum, in! Lorem ipsum dolor sit amet, consectetur
+              adipisicing elit. Magnam numquam pariatur itaque, vel quis sequi
+              magni harum minus nulla molestias quibusdam veritatis corporis
+              excepturi architecto non dolore nisi ratione expedita.
+            </p>
+            <div class="btn-container">
+              <button class="btn">Saiba Mais</button>
+            </div>
           </div>
-          <img src="@/assets/images/espelho.jpg" alt="">
+          <img src="@/assets/images/espelho.jpg" alt="" />
         </div>
       </div>
-
     </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style scoped>
 .container-servicos {
   width: 100vw;
-  background: #0B232E;
+  background: #0b232e;
 }
 .titulo-servicos {
   text-align: center;
@@ -69,7 +105,7 @@ export default {
   max-width: 1200px;
   margin: 80px auto;
 }
-.servicos-itens .infos-img{
+.servicos-itens .infos-img {
   display: flex;
 }
 .servicos-itens .item p {
