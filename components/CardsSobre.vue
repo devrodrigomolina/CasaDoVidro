2693690b2920e268c46916e5d3e0673c0146960d
FOXXX <75998678+FOXXX2K@users.noreply.github.com>
2022-08-13 23:10:06 -0300

    Criado componente cards

---
+++ components/CardsSobre.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="cards" >
+    <div class="card" v-for="card in imgCard" :key="card">
+      <img class="img-card" :src="card.img" alt="">
+      <h3 class="text-card">{{ card.text }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      imgCard: [
+        {
+          img: require('@/assets/images/shield.png'),
+          text: '3 Anos de garantia de serviço'
+        },
+        {
+          img: require('@/assets/images/shield-1.png'),
+          text: '5 Anos de garantia da fabrica'
+        },
+        {
+          img: require('@/assets/images/credit-card.png'),
+          text: 'Pagamento em até 12x sem juros'
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+
+</style>
